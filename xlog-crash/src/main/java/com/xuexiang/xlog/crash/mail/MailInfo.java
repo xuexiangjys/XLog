b4a5b9a4c41d573b64d162c9e4cec624c852c27d
xuexiangjys <xuexiangjys@163.com>
2019-03-18 01:39:23 +0800

    解决邮件发送附件不成功的问题

--- xlog-crash/src/main/java/com/xuexiang/xlog/crash/mail/MailInfo.java
+++ xlog-crash/src/main/java/com/xuexiang/xlog/crash/mail/MailInfo.java
@@ -202,7 +202,7 @@ public class MailInfo extends Authenticator implements Parcelable {
         mc.addMailcap("text/html;; x-java-content-handler=com.sun.mail.handlers.text_html");
         mc.addMailcap("text/xml;; x-java-content-handler=com.sun.mail.handlers.text_xml");
         mc.addMailcap("text/plain;; x-java-content-handler=com.sun.mail.handlers.text_plain");
-        mc.addMailcap("mMultipart/*;; x-java-content-handler=com.sun.mail.handlers.multipart_mixed");
+        mc.addMailcap("multipart/*;; x-java-content-handler=com.sun.mail.handlers.multipart_mixed");
         mc.addMailcap("message/rfc822;; x-java-content-handler=com.sun.mail.handlers.message_rfc822");
         CommandMap.setDefaultCommandMap(mc);
         mProps = new Properties();
